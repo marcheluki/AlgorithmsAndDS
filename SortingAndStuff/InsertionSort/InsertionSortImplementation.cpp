02dd27d8be2015d9eb9993e53d4639efeb6998e6
Marcheluki <a01637239@tec.mx>
2024-08-08 07:52:43 -0600

    comments added for insertionSort

--- SortingAndStuff/InsertionSort/InsertionSortImplementation.cpp
+++ SortingAndStuff/InsertionSort/InsertionSortImplementation.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void InsertionSort(int arr[], int n){
-    for (int i=1; i<n; i++){
-        int key = arr[i];
-        int j = i-1;
-        while (j>=0 && arr[j]>key){
-            arr[j+1]=arr[j];
-            j=j-1;
+void InsertionSort(int arr[], int n){ // This function sorts the array in increasing order
+    for (int i=1; i<n; i++){ // it starts from the second element of the array because the first element is already sorted
+        int key = arr[i]; //key is element to be compared with the elements before it
+        int j = i-1; // j is the index of the element before the key element
+        while (j>=0 && arr[j]>key){ // this loop compares the key element with the elements before it
+            arr[j+1]=arr[j]; // if key is smaller than j, the element before it is shifted to the right by making 
+                //the element at j+1 equal to the element at j, and j becomes j-1, so the value of j is decremented
+            j=j-1; // the index is decremented to compare the key element with the element before the previous element
         }
-        arr[j+1] = key;
+        arr[j+1] = key; // the key element is placed in the correct position
     }
 };
 
--- SortingAndStuff/InsertionSort/instReverseImplement.cpp
+++ SortingAndStuff/InsertionSort/instReverseImplement.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void InsertionSortDecrease(int arr[], int n){
-    for(int i=1;i<n;i++){
+void InsertionSortDecrease(int arr[], int n){ // This function sorts the array in decreasing order
+    for(int i=1;i<n;i++){ //  it starts from the second element of the array because the first element is already sorted
         
-        int key= arr[i];
-        int j=i-1;
-        while(j>=0 && arr[j]<key){
-            arr[j+1]=arr[j];
-            j = j-1;
+        int key= arr[i]; // key is the element that is to be compared with the elements before it
+        int j=i-1; // j is the index of the element before the key element
+        while(j>=0 && arr[j]<key){ // this loop compares the key element with the elements before it
+            arr[j+1]=arr[j]; // if the key element is smaller than the element before it, the element before it is shifted to the right
+            j = j-1; // the index is decremented to compare the key element with the element before the previous element
         }
-        arr[j+1]=key;
+        arr[j+1]=key; // the key element is placed in the correct position
     }
 }
 
